naturalatlas/Leaflet

commit ad8fc9c36bc6846b486868a691b60f340ff63ffd

Fix(GridLayer): Do not fire 'tileload' event if tile src is empty image (#6025)
* do not fire 'load' event if tile src is the empty image (i.e. L.Util.emptyImageUrl)

* add TileLayer setUrl test

src/layer/tile/GridLayer.js
@@ -842,7 +842,7 @@ export var GridLayer = Layer.extend({
 	},
 
 	_tileReady: function (coords, err, tile) {
-		if (!this._map) { return; }
+		if (!this._map || tile.getAttribute('src') === Util.emptyImageUrl) { return; }
 
 		if (err) {
 			// @event tileerror: TileErrorEvent

spec/suites/layer/tile/TileLayerSpec.js
@@ -455,4 +455,27 @@ describe('TileLayer', function () {
 			});
 		}
 	});
+
+	describe('#setUrl', function () {
+		it('fires only one load event', function (done) {
+			var layer = L.tileLayer(placeKitten).addTo(map);
+			var counts = {
+				load: 0,
+				tileload: 0
+			};
+			map.setView([0, 0], 1);
+
+			layer.on('tileload load', function (e) {
+				counts[e.type]++;
+			});
+
+			layer.setUrl(placeKitten);
+
+			setTimeout(function () {
+				expect(counts.load).to.equal(1);
+				expect(counts.tileload).to.equal(8);
+				done();
+			}, 250);
+		});
+	});
 });

commit 12639b804a0e7f2d8023fab4a0dc1e57743af010

Fixed regression caused by https://github.com/Leaflet/Leaflet/commit/ad8fc9c36bc6846b486868a691b60f340ff63ffd (tile is not always set)

src/layer/tile/GridLayer.js
@@ -843,7 +843,7 @@ export var GridLayer = Layer.extend({
 	},
 
 	_tileReady: function (coords, err, tile) {
-		if (!this._map || tile.getAttribute('src') === Util.emptyImageUrl) { return; }
+		if (!this._map || (tile && tile.getAttribute('src') === Util.emptyImageUrl)) { return; }
 
 		if (err) {
 			// @event tileerror: TileErrorEvent